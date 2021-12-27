b20a1efaaf7164525438fa480c573b1b3955a874
okashogo <cocorusakana@gmail.com>
2021-12-27 13:46:55 +0900

    add: cdk: dynamoDB connect

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Table, AttributeType } from "@aws-cdk/aws-dynamodb";
+import { AssetCode, Function, Runtime } from "@aws-cdk/aws-lambda";
+import {
+  Cors,
+  RestApi,
+  LambdaIntegration,
+  IResource,
+  MockIntegration,
+  PassthroughBehavior,
+} from "@aws-cdk/aws-apigateway";
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const dynamoTable = new Table(this, "LyricsQuiz", {
+      partitionKey: {
+        name: "title",
+        type: AttributeType.STRING,
+      },
+      tableName: "quizzes",
+      removalPolicy: cdk.RemovalPolicy.RETAIN, // NOT recommended for production code
+    });
 
     // The code that defines your stack goes here
 
